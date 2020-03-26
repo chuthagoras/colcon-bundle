1c1c35f01cee952a9ba184ca37bbf8a2dff863f7
Chuta Sano <chuta_japan@comcast.net>
2020-03-26 11:49:28 -0400

    space

--- colcon_bundle/installer/pip3.py
+++ colcon_bundle/installer/pip3.py
@@ -10,19 +10,19 @@ from colcon_bundle.installer.base_pip_installer import \
 class Pip3BundleInstallerExtensionPoint(BasePipInstallerExtensionPoint):
     """Python 3 pip installer."""
 
-   def __valid_python3_version(version_string):
-       try:
-           version_float = float(version_string)
-       except ValueError:
-           raise parser.ArgumentTypeError(version_string + " cannot be "
-           "converted to a float... Make sure it is of form 3.X")
-       if version_float < 3.4:
-           raise parser.ArgumentTypeError("Only 3.4 and higher are "
-           "supported")
-       if version_float >= 4:
-           raise parser.ArgumentTypeError("Only 3.X are supported")
-       return version_string
-
+    def __valid_python3_version(version_string):
+        try:
+            version_float = float(version_string)
+        except ValueError:
+            raise parser.ArgumentTypeError(version_string + " cannot be "
+            "converted to a float... Make sure it is of form 3.X")
+        if version_float < 3.4:
+            raise parser.ArgumentTypeError("Only 3.4 and higher are "
+            "supported")
+        if version_float >= 4:
+            raise parser.ArgumentTypeError("Only 3.X are supported")
+        return version_string
+ 
     def add_arguments(self, *, parser):  # noqa: D102
         parser.add_argument(
             '--pip3-args',
