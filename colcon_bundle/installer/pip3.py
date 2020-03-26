399db3afaff64bdb1aea8324ad6443109aadc047
Chuta Sano <chuta_japan@comcast.net>
2020-03-26 10:04:18 -0400

    Added a --python3-version flag to support a custom python3 version (eg: 3.8). Until this commit, only 3.5 was supported

--- colcon_bundle/installer/pip3.py
+++ colcon_bundle/installer/pip3.py
@@ -21,10 +21,27 @@ class Pip3BundleInstallerExtensionPoint(BasePipInstallerExtensionPoint):
             '--pip3-requirements', type=str, default=None,
             help='Path to a requirements.txt. All packages in the file'
                  'will be installed into Python3 in the bundle')
+        def _valid_python3_version(version_string):
+            try:
+                version_float = float(version_string)
+            except ValueError:
+                raise parser.ArgumentTypeError(version_string + " cannot be
+                converted to a float... Make sure it is of form 3.X")
+            if version_float < 3.4:
+                raise parser.ArgumentTypeError("Only 3.4 and higher are
+                supported")
+            if version_float >= 4:
+                raise parser.ArgumentTypeError("Only 3.X are supported")
+            return version_string
+        parser.add_argument(
+            '--python3-version', type=str, default='3.5',
+            help='Python3 version number. Default is 3.5, but can be one of'
+            '3.4, 3.5, and so on')
 
     def initialize(self, context):  # noqa: D102
         super().initialize(context)
         self._python_path = os.path.join(
-            self.context.prefix_path, 'usr', 'bin', 'python3')
+            self.context.prefix_path, 'usr', 'bin', ('python' +
+                self.context.args.python3_version))
         self._pip_args = self.context.args.pip3_args
         self.additional_requirements = self.context.args.pip3_requirements
