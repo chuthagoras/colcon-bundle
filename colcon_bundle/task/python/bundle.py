5c9224817f0480468fb0b5c7d7291e833d20879c
Chuta Sano <chuta_japan@comcast.net>
2020-04-10 11:37:33 -0400

    Added python3 minor version specification

--- colcon_bundle/task/python/bundle.py
+++ colcon_bundle/task/python/bundle.py
@@ -61,6 +61,6 @@ class PythonBundleTask(TaskExtensionPoint):
                 )
 
             # TODO: The Pip managers should be doing this
-            apt = args.installers['apt']
-            apt.add_to_install_list('libpython3-dev')
-            apt.add_to_install_list('python3-pip')
+            #apt = args.installers['apt']
+            #apt.add_to_install_list('libpython3-dev')
+            #apt.add_to_install_list('python3-pip')
